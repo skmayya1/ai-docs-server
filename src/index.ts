9d63da7865904f45ce368cd2810bb602c62a2baf
skmayya1 <skandamayya4@gmail.com>
2025-06-25 23:14:50 +0530

    fix:origin

--- src/index.ts
+++ src/index.ts
@@ -10,22 +10,14 @@ const PORT = process.env.PORT || 5000;
 
 app.use(express.json());
 
-const allowedOrigins = [
-  'http://localhost:3000',
-  'https://ai-word-doc.vercel.app'
-];
+
 
 app.use(cors({
-  origin: (origin, callback) => {
-    if (!origin) return callback(null, true);
-    if (allowedOrigins.includes(origin)) {
-      return callback(null, true);
-    }
-    return callback(new Error('Not allowed by CORS'));
-  },  credentials: true,
+  origin: 'https://ai-word-doc.vercel.app'
+  , credentials: true,
 }));
 
-app.use('/api',Router);
+app.use('/api', Router);
 
 app.listen(PORT, () => {
   console.log(`Server running at http://localhost:${PORT}`);
